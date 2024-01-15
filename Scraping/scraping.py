524ab7233d10787a599b398089a00e72a0543823
早川ふたば <s2322052@stu.musashino-u.ac.jp>
2024-01-15 14:23:25 +0900

    細かいところを変更

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -5,7 +5,7 @@ import csv
 import re
 from datetime import datetime
 
-# スクレイピング機能
+# スクレイピング
 def scrape_weather_data(url):
     try:
         response = requests.get(url)
@@ -23,8 +23,6 @@ def scrape_weather_data(url):
     for row in data_rows:
         columns = row.find_all('td')
         if len(columns) >= 11:
-            # 日付フォーマットを YYYY-MM-DD 形式に変更する場合はここを修正
-            # 例: date = datetime.strptime(columns[0].text.strip(), '%Y/%m/%d').strftime('%Y-%m-%d')
             date = columns[0].text.strip()
             temperature = columns[10].text.strip() + "℃"  
             pressure = columns[1].text.strip() + " hPa" 
@@ -33,7 +31,7 @@ def scrape_weather_data(url):
 
     return temperature_data, pressure_data
 
-# データベース関連の機能
+# データベース
 def create_database():
     with sqlite3.connect('weather_data.db') as conn:
         cursor = conn.cursor()
@@ -61,12 +59,12 @@ def read_data_from_database(table_name):
         cursor.execute(f'SELECT * FROM {table_name}')
         return cursor.fetchall()
 
-# CSV出力機能
+# CSV出力
 def save_data_to_csv(data, filename):
     try:
         with open(filename, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerow(['Date', 'Value'])  # CSVのヘッダー
+            writer.writerow(['Date', 'Value'])  
             writer.writerows(data)
     except IOError as e:
         print(f"IOError while writing to {filename}: {e}")
@@ -75,13 +73,13 @@ def save_data_to_csv(data, filename):
 def main():
     create_database()
 
-    # 1つ目のURLからデータを取得
+    # 1つ目からデータを取得
     url1 = 'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year=2023&month=12&day=&view=a2'
     temperature_data1, pressure_data1 = scrape_weather_data(url1)
     insert_data_into_database(temperature_data1, 'temperature')
     insert_data_into_database(pressure_data1, 'pressure')
 
-    # 2つ目のURLからデータを取得
+    # 2つ目からデータを取得
     url2 = 'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php?prec_no=51&block_no=47636&year=2024&month=01&day=06&view=a1'
     temperature_data2, pressure_data2 = scrape_weather_data(url2)
     insert_data_into_database(temperature_data2, 'temperature')
